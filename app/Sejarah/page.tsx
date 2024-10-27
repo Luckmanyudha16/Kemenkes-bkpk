da6e55ec13e4b9f239abe6c37089d9ef8f46cb74
Luckmanyudha16 <luckmanyudha1603@gmail.com>
2024-10-27 17:03:08 +0700

    awal

---
+++ app/Sejarah/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import style  from "./PageSejarah.module.css";
+// import Link from "next/link";
+
+export default function Sejarah() {
+  return (
+   <div className={style.bgRed}>SEJARAH</div>
+  );
+}
