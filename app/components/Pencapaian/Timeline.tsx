ef81f2a4f66acfbae6d9b829d87de3de3d0ce41e
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-11-06 15:48:00 +0700

    fixed: pop up close button not showed up after deployment

--- app/components/Pencapaian/Timeline.tsx
+++ app/components/Pencapaian/Timeline.tsx
@@ -359,7 +359,7 @@ export default function Timeline() {
       {isPopupVisible && selectedIndex !== null && (
         <div className="absolute bg-black bg-opacity-50 w-[100%] h-[100%] z-[999] left-0 top-0 flex items-center justify-center">
           <div className={`flex ${data[selectedIndex].media[currentMediaIndex].type == 'video' ? "flex-col" : ""} items-center px-[80px] py-[30px] max-w-[60vw] min-h-[60vh] bg-white border border-sm rounded-xl gap-8 relative`}>
-            <div className='absolute right-[-20] top-[-20] bg-white border w-[50px] h-[50px] rounded-full flex items-center justify-center cursor-pointer hover:-translate-y-1 hover:scale-110 duration-300' onClick={handleClosePopup} style={{ color: "green"}}>
+            <div className='absolute right-[-20px] top-[-20px] bg-white border w-[50px] h-[50px] rounded-full flex items-center justify-center cursor-pointer hover:-translate-y-1 hover:scale-110 duration-300' onClick={handleClosePopup} style={{ color: "green"}}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
               </svg>
